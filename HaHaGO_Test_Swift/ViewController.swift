d65c0568a18669d9c09a3aee3249f0386373523e
nerochiu <kidchiou2011@gmail.com>
2018-03-09 12:01:06 +0800

    Initial Commit

---
+++ HaHaGO_Test_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HaHaGO_Test_Swift
+//
+//  Created by nerochiu on 2018/3/9.
+//  Copyright © 2018年 ISCOM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
